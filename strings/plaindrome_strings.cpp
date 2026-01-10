1ecd7544e9120616e01dff0a82b5c33df6053eda
21rawatk-netizen <21rawatk@gmail.com>
2026-01-10 16:11:44 +0530

    Update plaindrome_strings.cpp

--- strings/plaindrome_strings.cpp
+++ strings/plaindrome_strings.cpp
@@ -1,1 +1,44 @@
-
+//plaindrome of string
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s;
+    cout<<"enter a string";
+    cin>>s;
+int start=0;//starting index of string s
+int end= s.length()-1;//last index of string s
+bool Palindrome =true;//asuume the string is an palindrome
+while(start<end){
+    if(s[start]!= s[end]){
+         Palindrome=false;
+        break;
+    }
+    start++;//move forward in a string
+    end--;//move backward in string
+}
+if ( Palindrome )
+    cout<<"Palindrome";
+else
+    cout<<"not a Palindrome"<<endl;
+//palindrome of integer
+int x;
+int original=x;
+long long rev=0;
+cout<<"Enter the integer";
+cin>>x;
+if(x<0){
+    return false;
+}
+while(x>0){
+int digit=x%10;
+rev=rev*10+digit;
+x=x/10;
+}
+if(original==rev){
+    return true;
+}else{
+    return false;
+}
+    return 0;
+}
